40d2e25e36674af7867770a68085b58e3bebbd18
liszd <lidzd1992@gmail.com>
2014-07-12 23:08:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+Douban_Group_Img
+================
+
+Actually, I rarely used Douban Group, and not login douban frequently. But Oneday, I met the douban Group, and found a group called `SHY` is very intesting. 
+
+In the group, everyone can upload any beautiful pictures, like self-timer. So I write a crawler to download the pictures.
+
+A picture downloaded from group
+
+![image](http://naalnblog-wordpress.stor.sinaapp.com/uploads/2014/07/p16081849.jpg)
+
+
+``` 
+●　要有胸，要有腿，要有翘臀，要有嘴。要有锁骨儿，要有美。
+●　要有型，要有款，要有胸肌，要有块。要有胡渣儿，要有范。
+●　虽然马赛克是阻止人类文明进步的绊脚石，但是请不要忽略的它神圣的存在。
+```
+
+Otaku saying beauty is the first productivity. When I accustomed to 1024, I go to Douban see artsy chick. So I spent a morning wrote a reptile. 
+
+The scrip is mush easy, just login, crawl group, download pictures.
+
+Before runing, you need modify the code such as username, password, all_pages.
+
+
+---
+License
+---
+Copyright (c) 2014 	liszd1992@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 # coding=utf-8
-
-
 import urllib
 import urllib2
 import cookielib
@@ -27,9 +25,6 @@ def post(url, data, cj):
     if debug >= 1:
         print 'POST_REQUEST:\n' + url
         print 'POST_CONTENT:\n' + data
-        # print
-        # 'POST_RESPONSE:\n'+replace_all(response.read(),{'\n':'','\t':'','
-        # ':'','　':''})
     return response.read()
 
 
@@ -45,30 +40,28 @@ def browse(url, cj):
     except:
         return ''
 
+
 def downImg(url, name):
     fr = urllib.urlopen(url)
     stream = fr.read(-1)
     fr.close()
-    fw = open('/Users/liam/Dropbox/Project/Douban_group/picture/' + name, 'w')
+    fw = open('picture/' + name, 'w')
     fw.write(stream)
     fw.close()
 
 
 def main():
     du = 'http://m.douban.com'
-    # 设置区域
+
     # set Group ID
     group_id = 'haixiuzu'
     # set refresh_interval
     refresh_interval = 1
-    # set sofa Content
-    sofa_content = '越过山丘 才发现无人等候 喋喋不休 再也唤不回温柔'
+    # set form_email, and form_password
     data = {'form_email': 'liamzhuce@gmail.com',
-            'form_password': '1qaZ@wsx', 'action': '/'}
-    # 到这就不要修改了，除非你知道你在干吗
-
-    # FIXME Why need one more Login, wired?
-    # if browse(du,cj)
+            'form_password': '********', 'action': '/'}
+    # set group all_pages
+    all_pages = 2000
 
     # login
     try:
@@ -77,8 +70,8 @@ def main():
         print 'FATAL'
         exit(0)
     times = 0
-    page = 2500
-    for page in range(1, 1797)[::-1]:
+    
+    for page in range(1, all_pages)[::-1]:
         # Get Groups
         group_content = browse(
             du + '/group/' + group_id + '/topics?page=' + str(page), cj)
@@ -89,7 +82,7 @@ def main():
             print group_content
         if times == 0:
             group_title = re.findall('<title>(.*)<\/title>', group_content)[0]
-            print '\n-----抢"' + group_title + '"的沙发中-----'
+            print '\n-----download "' + group_title + '"ing-----'
             times += 1
         items = re.findall(
             '<ahref="\/group\/topic\/(\d+)\/\?session=\w+">([^<]+)<\/a><span>\((\d+)\)\|', group_content)
@@ -98,20 +91,14 @@ def main():
             # print items
             for i in items:
                 item_title = i[1]
-                item_title = item_title.replace('晒','')
-                item_title = item_title.replace('【】','')
-                item_title = item_title.replace('/','')
+                item_title = item_title.replace('/', '')
                 item_ID = i[0]
                 item_comm = i[2]
                 if int(item_comm) > 80:
-                    # print item_title + '[@' + datetime.now().isoformat() + ']'
-                    print item_title+' | '+item_comm
+                    print item_title + ' | ' + item_comm
                     item_url = du + '/group/topic/' + item_ID + '/'
                     item_content = browse(item_url, cj)
                     img_content = replace_all(item_content, replace_dict)
-                    # print img_content
-                    # imgs = re.findall(
-                    #     '<ahref="\/group\/topic\/\d+\/photo\?purl=[^<]+\&session=\w+"><imgsrc="([^<]*)"\/>', img_content)
                     imgs = re.findall(
                         '<imgsrc="([^<]*)\.jpg', img_content)
                     if not imgs.__len__() == 0:
@@ -121,20 +108,11 @@ def main():
                             img_url = img_url.replace('small', 'large')
                             filename = str(item_title) + str(num) + '.jpg'
                             try:
-                                downImg(img_url+'.jpg', filename)
+                                downImg(img_url + '.jpg', filename)
                             except:
                                 pass
                     time.sleep(refresh_interval * random.randint(2, 5))
 
-        #         if not re.match('captcha',item_content):
-        #             item_post_session = re.findall('name="session"value="([^"]+)"',replace_all(item_content,replace_dict))
-        # print item_post_session
-        #             item_post_session = item_post_session[0]
-        #             data = {'content':sofa_content,'action':'comments','session':item_post_session}
-        #             post(item_url+'comments',data,cj)
-        #         else:
-        #             print '有验证码啦！\a'
-        #             break
         if debug == 1:
             time.sleep(refresh_interval)
         else:
