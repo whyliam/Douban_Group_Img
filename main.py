ce84f870db851bb6daf54b7e40c8a2102e3f4c6c
whyliam <lidzd1992@gmail.com>
2015-08-11 20:21:26 -0400

    update

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def downImg(url, name):
     fw.close()
 
 
-def main():
+def main(user_name,user_password):
     du = 'http://m.douban.com'
 
     # set Group ID
@@ -58,10 +58,10 @@ def main():
     # set refresh_interval
     refresh_interval = 1
     # set form_email, and form_password
-    data = {'form_email': 'liamzhuce@gmail.com',
-            'form_password': '********', 'action': '/'}
+    data = {'form_email': user_name,
+            'form_password': user_password, 'action': '/'}
     # set group all_pages
-    all_pages = 2000
+    all_pages = 10
 
     # login
     try:
@@ -71,44 +71,42 @@ def main():
         exit(0)
     times = 0
     
-    for page in range(1, all_pages)[::-1]:
+    for page in range(1, all_pages):
         # Get Groups
         group_content = browse(
-            du + '/group/' + group_id + '/topics?page=' + str(page), cj)
+            du + '/group/' + group_id + '/topics?start=' + str(page*25), cj)
         print page
         replace_dict = {'\n': '', '\t': '', ' ': '', '　': ''}
         group_content = replace_all(group_content, replace_dict)
         if debug >= 1:
             print group_content
         if times == 0:
-            group_title = re.findall('<title>(.*)<\/title>', group_content)[0]
+            group_title = re.findall('<h1class="group-name">(.*)<\/h1>', group_content)[0]
             print '\n-----download "' + group_title + '"ing-----'
             times += 1
         items = re.findall(
-            '<ahref="\/group\/topic\/(\d+)\/">([^<]+)<\/a><span>\((\d+)\)\|', group_content)
-
+            '<ahref="\/group\/topic\/(\d+)\/"title="(.*?)"><.*?<divclass="info">(\d+)回应', group_content)
         if not items.__len__() == 0:
-            # print items
             for i in items:
                 item_title = i[1]
                 item_title = item_title.replace('/', '')
                 item_ID = i[0]
                 item_comm = i[2]
-                if int(item_comm) > 80:
+                if int(item_comm) > 1:
                     print item_title + ' | ' + item_comm
                     item_url = du + '/group/topic/' + item_ID + '/'
                     item_content = browse(item_url, cj)
                     img_content = replace_all(item_content, replace_dict)
                     imgs = re.findall(
-                        '<imgsrc="([^<]*)\.jpg', img_content)
+                        '<imgid=.*?src=\'([^<]*)\'/>', img_content)
                     if not imgs.__len__() == 0:
                         num = 0
                         for img_url in imgs:
+                            print img_url
                             num = num + 1
-                            img_url = img_url.replace('small', 'large')
                             filename = str(item_title) + str(num) + '.jpg'
                             try:
-                                downImg(img_url + '.jpg', filename)
+                                downImg(img_url, filename)
                             except:
                                 pass
                     time.sleep(refresh_interval * random.randint(2, 5))
@@ -122,4 +120,6 @@ def main():
 if __name__ == '__main__':
     cj = cookielib.CookieJar()
     debug = 0
-    main()
+    user_name = raw_input('豆瓣 用户名\n')
+    user_password = raw_input('\n豆瓣 密码\n')
+    main(user_name,user_password)
